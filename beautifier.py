ebe81212bdef7603512008ffdddef0095bd630b4
Léon McGregor <3817332+LonMcGregor@users.noreply.github.com>
2019-04-19 20:02:10 +0100

    process data

---
+++ beautifier.py
@@ -0,0 +1,41 @@
+
+# read from file
+# strip the first line
+# remove any "quotes"
+# change , into ","
+# write to file
+
+ALL_DATA = []
+DATA_FILES = [
+    ["category_113_feature-requests.csv", ""],
+    ["category_127_address-bar.csv", "ADDRESS BAR"],
+    ["category_128_bookmarks.csv", "BOOKMARKS"],
+    ["category_129_panels.csv", "PANELS"],
+    ["category_130_settings.csv", "SETTINGS"],
+    ["category_131_tabs.csv", "TABS"],
+    ["category_132_themes.csv", "THEMES"],
+    ["category_136_mobile.csv", "MOBILE"]
+]
+
+def readAFile(filename, base_tag):
+    with open(filename, "r", encoding="utf-8") as datafile:
+        datafile.readline() # ditch the first line
+        line = ""
+        for line in datafile:
+            csv = line.strip()
+            csv = csv.replace("\"", "'")
+            items = csv.split(",")
+            tags = items[5].split(":")
+            if base_tag != "" and base_tag not in tags:
+                tags.append(base_tag)
+            topic = '"' + '","'.join(items[0:4]) + '",' + '["' + '","'.join(tags) + '"]'
+            ALL_DATA.append(topic)
+
+for file in DATA_FILES:
+    readAFile(file[0], file[1])
+
+with open("data.js", "w", encoding="utf-8") as datafile:
+    datafile.write("const DATA = [\n")
+    for item in ALL_DATA:
+        datafile.write(item + ",\n")
+    datafile.write("];")
---
+++ scraper.js
@@ -0,0 +1,41 @@
+javascript:(function csv(){
+    "use strict";
+    const data = ["ID,Title,Author,Date,Votes,Tags"];
+    function csvOnePage(){
+        const topics = document.querySelectorAll(".topic-list li");
+        Array.from(topics).forEach(topic => {
+            const title = topic.querySelector(".title a").innerText.replace(/,/g, " ");
+            const tags = Array.from(topic.querySelectorAll(".tag"));
+            const author = topic.querySelector(".avatar .select > *").getAttribute("data-original-title");
+            const date = topic.querySelector(".timeago").getAttribute("datetime");
+            const votes = topic.querySelector(".stats > span").innerText;
+            const topicId = topic.getAttribute("data-tid");
+
+            const tagText = tags.reduce((prev, curr, index, arr) => prev + curr.innerText + ((index + 1 === arr.length) ? "" : ":"), "");
+            const topicCsv = `${topicId},${title},${author},${date},${votes},${tagText}`;
+            data.push(topicCsv);
+        });
+    }
+
+    function doAllPages(){
+        csvOnePage();
+        if(!document.querySelector(".next").classList.contains("disabled")){
+            document.querySelector(".next a").click();
+            setTimeout(doAllPages, 1000);
+        } else {
+            /* http://stackoverflow.com/questions/31048215/ddg#31048350 */
+            /* http://stackoverflow.com/questions/19327749/ddg#19328891 */
+            const datafile = new Blob([data.join("\n")+"\n"], {type: 'text/plain'});
+            const datafileurl = window.URL.createObjectURL(datafile);
+            const a = document.createElement("a");
+            document.body.appendChild(a);
+            a.style = "display: none";
+            a.href = datafileurl;
+            a.download = window.location.pathname.replace("/", "_")+".csv";
+            a.click();
+        }
+    }
+
+    doAllPages();
+
+})();
